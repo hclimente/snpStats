51d43cfc9bce74709f67704eca787b977486b3c7
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-12 19:48:56 +0000

    don't use outdated library.dynam
    - remove zzz.R, and from Collate field
    - useDynLoad() in NAMESPACE
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/snpStats@59127 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onLoad <- function(libname, package) {
-  library.dynam("snpStats", package)
-  methods:::bind_activation(TRUE)
-}
-
-.Last.lib <- function(libname, package) {
-  methods:::bind_activation(FALSE)
-}
